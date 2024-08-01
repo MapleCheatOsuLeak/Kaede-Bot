3e665f9a39b7237cd7c99e519e099e9274499e61
mrflashstudio <mrflashstudio@gmail.com>
2024-08-01 15:54:08 +0300

    Updated ServerRoles configuration

--- Kaede-Bot/Configuration/ServerRoles.cs
+++ Kaede-Bot/Configuration/ServerRoles.cs
@@ -2,6 +2,11 @@
 
 public class ServerRoles
 {
+    public ulong AdministratorRoleId { get; set; }
+    public ulong ModeratorRoleId { get; set; }
+    public ulong SupportRoleId { get; set; }
+    public ulong MediaRoleId { get; set; }
+    public ulong ResellerRoleId { get; set; }
     public ulong PremiumRoleId { get; set; }
     public ulong GiveawaysRoleId { get; set; }
 }
