b642e21008599606dff120c6427c058e7e168aa5
mrflashstudio <mrflashstudio@gmail.com>
2024-08-01 14:32:43 +0300

    Implemented tool calling for GPT module

---
+++ Kaede-Bot/Models/Web/ProductsInfoModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Kaede_Bot.Models.Web;
+
+public class ProductsInfoModel
+{
+    [JsonPropertyName("code")]
+    public int Code { get; set; }
+    public List<ProductsInfoEntry> Products { get; set; }
+}
+
+public class ProductsInfoEntry
+{
+    public string Name { get; set; }
+    public List<ProductsPriceEntry> Prices { get; set; }
+}
+
+public class ProductsPriceEntry
+{
+    public string Duration { get; set; }
+    public int Price { get; set; }
+    public int IsAvailable { get; set; }
+}
--- Kaede-Bot/Services/GPTService.cs
+++ Kaede-Bot/Services/GPTService.cs
@@ -1,10 +1,12 @@
 ﻿using System.Net.Http.Json;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using Discord;
 using Discord.Commands;
 using Kaede_Bot.Configuration;
 using Kaede_Bot.Database;
 using Kaede_Bot.Models.Database;
+using Kaede_Bot.Models.Web;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.DependencyInjection;
 using OpenAI;
@@ -15,11 +17,51 @@ namespace Kaede_Bot.Services;
 
 public class GPTService
 {
-    private const string Endpoint = "https://api.openai.com/v1/chat/completions";
     private static readonly SemaphoreSlim PromptAsyncLock = new(1, 1);
+    
+    private static readonly ChatTool getArticleListTool = ChatTool.CreateFunctionTool(
+        functionName: nameof(getArticleList),
+        functionDescription: "Retrieves article filename list. Articles contain all sorts of information about Maple. For example, information about the Maple project, various tutorials, set up guides, internal policies, troubleshooting guides, rules, contacts, and other useful information."
+    );
+    
+    private static readonly ChatTool getArticlesTool = ChatTool.CreateFunctionTool(
+        functionName: nameof(getArticles),
+        functionDescription: "Retrieves an array of articles.",
+        functionParameters: BinaryData.FromString("""
+        {
+          "type": "object",
+          "properties": {
+              "fileNames": {
+                  "type": "array",
+                  "description": "Filenames of articles to retrieve.",
+                  "items": {
+                      "type": "string"
+                  }
+              }
+          },
+          "required": [ "fileNames" ]
+        }
+        """)
+    );
+    
+    private static readonly ChatTool getMapleUserInfoTool = ChatTool.CreateFunctionTool(
+        functionName: nameof(getMapleUserInfo),
+        functionDescription: "Retrieves information about this user's Maple account. This information includes Maple User Id, Join date, Software subscriptions and their expiration."
+    );
+    
+    private static readonly ChatTool getMapleProductsInfoTool = ChatTool.CreateFunctionTool(
+        functionName: nameof(getMapleProductsInfo),
+        functionDescription: "Retrieves information about existing Maple products and their prices in EUR."
+    );
+    
+    private static readonly ChatTool getMapleStatusTool = ChatTool.CreateFunctionTool(
+        functionName: nameof(getMapleStatus),
+        functionDescription: "Retrieves online user count and status information about every Maple product (0 = Undetected, 1 = Outdated, 2 = Detected, 3 = Unknown)."
+    );
     
     private readonly IServiceProvider _services;
     private readonly KaedeDbContext _kaedeDbContext;
+    private readonly HttpClient _httpClient;
     
     private Encoder _tokenEncoder;
     private ChatClient _chatClient;
@@ -39,6 +81,7 @@ public class GPTService
     {
         _services = services;
         _kaedeDbContext = _services.GetRequiredService<KaedeDbContext>();
+        _httpClient = services.GetRequiredService<HttpClient>();
     }
 
     public Task Initialize()
@@ -121,43 +164,99 @@ public class GPTService
 
                 await _kaedeDbContext.SaveChangesAsync();
 
-                var chatCompletion = await _chatClient.CompleteChatAsync(messages, new ChatCompletionOptions
+                var requiresAction = true;
+                while (requiresAction)
                 {
-                    MaxTokens = _maxInputTokens + _maxOutputTokens,
-                    Temperature = _temperature,
-                    TopP = _topP,
-                    FrequencyPenalty = _frequencyPenalty,
-                    PresencePenalty = _presencePenalty
-                });
+                    var chatCompletion = await _chatClient.CompleteChatAsync(messages, new ChatCompletionOptions
+                    {
+                        Tools = { getArticleListTool, getArticlesTool, getMapleUserInfoTool, getMapleProductsInfoTool, getMapleStatusTool },
+                        MaxTokens = _maxInputTokens + _maxOutputTokens,
+                        Temperature = _temperature,
+                        TopP = _topP,
+                        FrequencyPenalty = _frequencyPenalty,
+                        PresencePenalty = _presencePenalty
+                    });
 
-                switch (chatCompletion.Value.FinishReason)
-                {
-                    case ChatFinishReason.Stop:
-                    case ChatFinishReason.Length:
-                        messages.Add(new AssistantChatMessage(chatCompletion));
+                    switch (chatCompletion.Value.FinishReason)
+                    {
+                        case ChatFinishReason.Stop:
+                        case ChatFinishReason.Length:
+                            requiresAction = false;
+                            
+                            messages.Add(new AssistantChatMessage(chatCompletion));
 
-                        _kaedeDbContext.GPTMessages.Add(new GPTMessageModel
-                        {
-                            Id = Guid.NewGuid(),
-                            UserId = context.User.Id,
-                            Role = "assistant",
-                            Content = chatCompletion.Value.ToString(),
-                            Timestamp = DateTime.UtcNow.AddMilliseconds(100)
-                        });
+                            _kaedeDbContext.GPTMessages.Add(new GPTMessageModel
+                            {
+                                Id = Guid.NewGuid(),
+                                UserId = context.User.Id,
+                                Role = "assistant",
+                                Content = chatCompletion.Value.ToString(),
+                                Timestamp = DateTime.UtcNow.AddMilliseconds(100)
+                            });
 
-                        await _kaedeDbContext.SaveChangesAsync();
+                            await _kaedeDbContext.SaveChangesAsync();
 
-                        await context.Message.ReplyAsync(chatCompletion.Value.ToString().Replace("@everyone", "@ everyone").Replace("@here", "@ here"));
-                        break;
-                    case ChatFinishReason.ContentFilter:
-                        await context.Message.ReplyAsync("Filtered.");
-                        break;
-                    case ChatFinishReason.ToolCalls:
-                        break;
-                    case ChatFinishReason.FunctionCall:
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException("ChatCompletion.FinishReason");
+                            await context.Message.ReplyAsync(chatCompletion.Value.ToString().Replace("@everyone", "@ everyone").Replace("@here", "@ here"));
+                            break;
+                        case ChatFinishReason.ContentFilter:
+                            await context.Message.ReplyAsync("Filtered.");
+                            requiresAction = false;
+                            break;
+                        case ChatFinishReason.ToolCalls:
+                            messages.Add(new AssistantChatMessage(chatCompletion));
+                            
+                            foreach (var toolCall in chatCompletion.Value.ToolCalls)
+                            {
+                                switch (toolCall.FunctionName)
+                                {
+                                    case nameof(getArticleList):
+                                    {
+                                        var toolResult = getArticleList();
+                                        messages.Add(new ToolChatMessage(toolCall.Id, toolResult));
+                                        break;
+                                    }
+                                    case nameof(getArticles):
+                                    {
+                                        using var argumentsJson = JsonDocument.Parse(toolCall.FunctionArguments);
+                                        var hasFileName = argumentsJson.RootElement.TryGetProperty("fileNames", out var fileName);
+                                        if (!hasFileName)
+                                            throw new ArgumentNullException(nameof(fileName), "The fileNames argument is required.");
+                                        
+                                        var toolResult = getArticles(fileName.EnumerateArray().Select(a => a.ToString()).ToArray());
+                                        messages.Add(new ToolChatMessage(toolCall.Id, toolResult));
+                                        break;
+                                    }
+                                    case nameof(getMapleUserInfo):
+                                    {
+                                        var toolResult = await getMapleUserInfo(context.User.Id);
+                                        messages.Add(new ToolChatMessage(toolCall.Id, toolResult));
+                                        break;
+                                    }
+                                    case nameof(getMapleProductsInfo):
+                                    {
+                                        var toolResult = await getMapleProductsInfo();
+                                        messages.Add(new ToolChatMessage(toolCall.Id, toolResult));
+                                        break;
+                                    }
+                                    case nameof(getMapleStatus):
+                                    {
+                                        var toolResult = await getMapleStatus();
+                                        messages.Add(new ToolChatMessage(toolCall.Id, toolResult));
+                                        break;
+                                    }
+                                    default:
+                                    {
+                                        throw new NotImplementedException("Unknown tool call.");
+                                    }
+                                }
+                            }
+                            break;
+                        case ChatFinishReason.FunctionCall:
+                            requiresAction = false;
+                            break;
+                        default:
+                            throw new NotImplementedException(chatCompletion.Value.FinishReason.ToString());
+                    }
                 }
             }
         }
@@ -165,5 +264,54 @@ public class GPTService
         {
             PromptAsyncLock.Release();
         }
+    }
+
+    private string getArticleList()
+    {
+        return Directory.Exists("Articles") ? JsonSerializer.Serialize(Directory.EnumerateFiles("Articles").Select(Path.GetFileNameWithoutExtension).ToArray()) : "[]";
+    }
+
+    private string getArticles(string[] fileNames)
+    {
+        return JsonSerializer.Serialize((from fileName in fileNames select $@"Articles\{fileName}.txt" into articlePath where File.Exists(articlePath) select File.ReadAllText(articlePath)).ToArray());
+    }
+    
+    private async Task<string> getMapleUserInfo(ulong userId)
+    {
+        var response = await _httpClient.GetAsync($"https://maple.software/backend/api/discord?t=0&u={userId}");
+        if (!response.IsSuccessStatusCode)
+            return "Server error.";
+        
+        var userinfo = await response.Content.ReadFromJsonAsync<UserInfoModel>();
+        if (userinfo is { Code: 0 })
+            return await response.Content.ReadAsStringAsync();
+            
+        return "Discord account is not linked to Maple account.";
+    }
+    
+    private async Task<string> getMapleProductsInfo()
+    {
+        var response = await _httpClient.GetAsync($"https://maple.software/backend/api/discord?t=4");
+        if (!response.IsSuccessStatusCode)
+            return "Server error.";
+        
+        var userinfo = await response.Content.ReadFromJsonAsync<ProductsInfoModel>();
+        if (userinfo is { Code: 0 })
+            return await response.Content.ReadAsStringAsync();
+            
+        return "Server error.";
+    }
+
+    private async Task<string> getMapleStatus()
+    {
+        var response = await _httpClient.GetAsync($"https://maple.software/backend/api/discord?t=3");
+        if (!response.IsSuccessStatusCode)
+            return "Server error.";
+        
+        var userinfo = await response.Content.ReadFromJsonAsync<ProductsInfoModel>();
+        if (userinfo is { Code: 0 })
+            return await response.Content.ReadAsStringAsync();
+            
+        return "Server error.";
     }
 }
